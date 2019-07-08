22e3294276fea77a144f89daf9657579ff83ed16
Puneet Arora <puneet.arora@moonraft.com>
2019-07-08 12:19:09 +0530

    Updates

---
+++ precache-manifest.6f20525c4a1c266190bb873c3b613e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e330be7f253d6e9e385806a332281b7d",
+    "url": "/letsWork/index.html"
+  },
+  {
+    "revision": "5d53e7cac04b9968514b",
+    "url": "/letsWork/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "83fabd55e32314d7ed53",
+    "url": "/letsWork/static/css/main.163ba624.chunk.css"
+  },
+  {
+    "revision": "5d53e7cac04b9968514b",
+    "url": "/letsWork/static/js/2.7758ab43.chunk.js"
+  },
+  {
+    "revision": "83fabd55e32314d7ed53",
+    "url": "/letsWork/static/js/main.408f8ad6.chunk.js"
+  },
+  {
+    "revision": "343fa099c035af41fa14",
+    "url": "/letsWork/static/js/runtime~main.6ec0cf19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6153593088f0d283b4557979c9521c12.js"
+  "/letsWork/precache-manifest.6f20525c4a1c266190bb873c3b613e7b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/letsWork/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
